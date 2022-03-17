7a8f357bfce69aeaff4416af41bd856f9e7fd0be
SeymurMs <tu7uykfbf@code.edu.az>
2022-03-17 10:57:10 +0400

    17Mart

---
+++ 17MartTask/Group.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _17MartTask
+{
+    class Group
+    {
+        private string _no;
+        private int _studentLimit;
+
+        public string No
+        {
+            get => this._no;
+            set
+            {
+                if (value.Length > 0 && CheckUpper(value))
+                {
+                    this._no = value;
+                }
+            }
+        }
+        public int StudentLimit
+        {
+            get => _studentLimit;
+            set
+            {
+                if (value >= 0)
+                {
+                    this._studentLimit = value;
+                }
+            }
+        }
+
+        public bool CheckUpper(string name)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                if (char.IsUpper(name[0]) && char.IsUpper(name[1]) && char.IsDigit(name[2]) && char.IsDigit(name[3]) && char.IsDigit(name[4]))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+        public string GetInfo()
+        {
+            return $"No: {this.No} - Name: {this.StudentLimit}";
+
+
+
+        }
+    }
+}
---
+++ 17MartTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _17MartTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Group group=new Group();
+
+            group.No = "AP2032";
+            group.StudentLimit = 18;
+            Console.WriteLine(group.No);
+            Console.WriteLine(group.StudentLimit);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
